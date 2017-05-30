a57b8083a9835b9889893ddaa1a8838212c2752d
Ramona Chantaf <ramonach.91@gmail.com>
2017-05-30 09:58:24 +0200

    Arbetar med elevsidan

---
+++ src/testverktygfrontend/FXMLStudentPageController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testverktygfrontend;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+/**
+ * FXML Controller class
+ *
+ * @author ramonachantaf
+ */
+public class FXMLStudentPageController implements Initializable {
+
+    @FXML
+    private Label userName;
+    @FXML
+    private Button logOut;
+    @FXML
+    private ListView<?> courses;
+    @FXML
+    private ListView<?> testsToDo;
+    @FXML
+    private ListView<?> testsDone;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
